4bfec1cb6810ed1e1745ae14d1217e9ac54dfa5f
junios-study <junios@naver.com>
2026-08-03 11:52:49 +0900

    AI 시작

---
+++ Source/L20260713_Day03/Zombie/ZombieAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ZombieAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20260713_DAY03_API UZombieAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
