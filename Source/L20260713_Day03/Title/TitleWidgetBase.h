b3358bde90b23b7870b9d1e3fc4ffdf1ffe85e1c
junios-study <junios@naver.com>
2026-08-10 14:23:09 +0900

    TitleWidget CPP

---
+++ Source/L20260713_Day03/Title/TitleWidgetBase.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleWidgetBase.generated.h"
+
+class UButton;
+class UEditableTextBox;
+
+
+/**
+ *
+ */
+UCLASS()
+class L20260713_DAY03_API UTitleWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeConstruct() override;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (WidgetBind))
+	TObjectPtr<UButton> StartServerButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (WidgetBind))
+	TObjectPtr<UButton> ConnectServerButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (WidgetBind))
+	TObjectPtr<UEditableTextBox> UserID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (WidgetBind))
+	TObjectPtr<UEditableTextBox> Password;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (WidgetBind))
+	TObjectPtr<UEditableTextBox> ServerIP;
+
+	UFUNCTION()
+	void StartServer();
+
+	UFUNCTION()
+	void ConnectServer();
+
+
+};
