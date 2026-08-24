223ed19b8e2d3aa9f7bcfa05d20905fe85d378c8
junios-study <junios@naver.com>
2026-08-24 10:19:31 +0900

    Lobby 창 만들기

---
+++ Source/L20260713_Day03/Lobby/LobbyWidgetBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20260713_DAY03_API ULobbyWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+	
+};
